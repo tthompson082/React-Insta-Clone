8b614f8b47cf8573384d233d0286399b4b10cd1d
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-03 16:50:04 -0400

    added state to App.js, map over state array to PostContainer.js

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -3,15 +3,33 @@ import './App.scss';
 
 import dummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+
+        <div className="cards">
+          {this.state.data.map(post =>(
+            <PostContainer
+              postProp={post}
+              key={Date.now()}
+            />
+          ))}
+        </div>
+      </div>
+    );
+    }
 }
 
 export default App;
