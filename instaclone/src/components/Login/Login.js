762b08a2f0e7465d1e17e843a5672d86f7e77262
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-05 16:25:51 -0400

    started styling login page

--- instaclone/src/components/Login/Login.js
+++ instaclone/src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Form, FormGroup, Container, Row, Col, Input} from 'reactstrap';
 
 class Login extends React.Component {
 
@@ -11,13 +12,23 @@ class Login extends React.Component {
         return (
             <div className="login">
 
-                <form onSubmit={this.login}>
-                    <input type="text" name="username" placeholder="username" />
-
-                    <input type="text" name="password" placeholder="password" />
-
-                    <button name="login-submit">Login</button>
-                </form>
+                <Container>
+                    <Form onSubmit={this.login}>
+                        <Col md={{size: 4, order: 1, offset: 4}}>
+                            <FormGroup>
+                                <Input type="username" name="username" placeholder="Username" />
+                            </FormGroup>
+                        </Col>
+                        <Col md={{size: 4, order: 1, offset: 4}}>
+                            <FormGroup>
+                                <Input type="password" name="password" placehoder="Password" />
+                            </FormGroup>
+                        </Col>
+                        <Col md={{size: 4, order: 1, offset: 4}}>
+                            <Button color="secondary" name="login-submit">Login</Button>
+                        </Col>
+                    </Form>
+                </Container>
             </div>
         )
     }
